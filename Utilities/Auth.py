b6581af6d8c1a0a4a745a89e3051818ff0776e0c
Nandra Rares <nandrarares16@gmail.com>
2022-07-31 14:33:04 +0300

    a

--- Utilities/Auth.py
+++
@@ -1,9 +0,0 @@
-import json
-
-
-# Returneaza Token ul si ClientID ul botului din folderul Secret (nepublicat pe Git)
-def GetDiscordBotCredentials():
-	CredentialsFile = open("Secret/DiscordBot.json", "r")
-	CredentialsJSON = json.load(CredentialsFile)
-	CredentialsFile.close()
-	return CredentialsJSON
